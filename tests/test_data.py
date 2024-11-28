215182774337bc017f210faf99a8c400935ff4e2
Katherina_Kaplina <kaplinakatrin@gmail.com>
2024-11-28 22:58:01 +0200

    added pytest marks

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,7 +1,10 @@
+import random
+
+random_number = random.randint(11, 100)
 TEST_DATA_CREATE_ACCOUNT = {
-    "first_name": "Kate_10",
-    "last_name": "Test_10",
-    "email": "kate_10@test.test",
+    "first_name": f"Kate_{random_number}",
+    "last_name": f"Test_{random_number}",
+    "email": f"kate_{random_number}@test.test",
     "password": "123QWqw!",
     "confirm_password": "123QWqw!"
 }
--- tests/test_sale_page.py
+++ tests/test_sale_page.py
@@ -1,25 +1,30 @@
 import allure
+import pytest
 
 
 @allure.title('Sale header')
+@pytest.mark.smoke
 def test_header_title(sale_page):
     sale_page.open_page()
     sale_page.check_page_header('Sale')
 
 
 @allure.title('Women’s Deals block subtitle')
+@pytest.mark.extended
 def test_women_deal_subtitle(sale_page):
     sale_page.open_page()
     sale_page.check_women_deal_subtitle('Pristine prices on pants, tanks and bras.')
 
 
 @allure.title('Women’s Deals block link is working')
+@pytest.mark.extended
 def test_women_deal_link(sale_page):
     sale_page.open_page()
     sale_page.check_women_deal_link('Women Sale')
 
 
 @allure.title('Women’s Deals menu jackets link is working')
+@pytest.mark.extended
 def test_menu_women_jackets_link(sale_page):
     sale_page.open_page()
     sale_page.check_menu_women_jackets_link()
